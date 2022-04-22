5f2c76b2ddb3bad3ff96b300ac2b31504a0f0429
Craig Buchanan <craig.buchanan@choa.org>
2022-04-22 09:06:19 -0500

    adding docker-compose artifacts

---
+++ postgres/initdb/initdb.sample.sql
@@ -0,0 +1,21 @@
+/*
+Purpose:
+    Create the pedscreen database, schema, and user
+Usage:
+    Change the file's name to be initdb.sql
+Reference:
+- https://github.com/chop-dbhi/ped-screen/blob/main/doc/Installing_Pedscreen.md#setting-up-the-pedscreen-database
+*/
+CREATE DATABASE pedscreen;
+
+-- TODO: make use of PEDSCREEN_PASSWORD environment variable, specified in .env file
+CREATE USER pedscreen WITH PASSWORD '[password here]';
+
+GRANT ALL ON DATABASE pedscreen TO pedscreen;
+
+\connect pedscreen;
+
+CREATE SCHEMA pedscreen;
+
+GRANT USAGE ON SCHEMA pedscreen to pedscreen;
+GRANT CREATE ON SCHEMA pedscreen to pedscreen;
