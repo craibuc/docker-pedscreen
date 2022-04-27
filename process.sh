ff14b4ba5bca3935be2676573b0d89038dc12aeb
Craig Buchanan <craig.buchanan@choa.org>
2022-04-27 11:58:16 -0500

    implementing docker run

--- process.sh
+++ process.sh
@@ -1,11 +1,10 @@
-# process all rows in the process.csv
+# process all rows in ./process.csv
 while IFS=, read -r department_id location_id date_start date_end; do
 
-  # skip header row
+  # skip header row and comments (#)
   if [ "$department_id" != "department_id" ] && [ ${department_id:0:1} != "#" ]; then
 
-    echo "docker run --rm --env-file=.env --volume $(pwd)/output:/app/output pedscreen:latest --department_id $department_id --location_id $location_id --date_start $date_start --date_end $date_end"
-    # docker run --rm --env-file=.env --volume $(pwd)/output:/app/output pedscreen:latest --department_id $department_id --location_id $location_id --date_start $date_start --date_end $date_end
+    docker run --rm --env-file=.env --volume $(pwd)/output:/app/output pedscreen:latest --department_id $department_id --location_id $location_id --date_start $date_start --date_end $date_end
 
   fi
 
